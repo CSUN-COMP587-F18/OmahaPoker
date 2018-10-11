daed531fbddca7f8715119890c0e2d2a023b7dc8
RobertBedrosian <chrisx32@gmail.com>
2018-10-10 23:52:45 -0700

    Added Unit tests for all poker hands

--- PokerTest.java
+++ PokerTest.java
@@ -74,5 +74,70 @@ public class HandTest {
         Poker.rank(numplayers,players);
         Assert.assertEquals(players.get(0).getrank(),8);
     }
+    @Test
+    public void testFlushOnePlayer(){
+        String [] args={"3c", "4c","5c","7s","Qs", "4h", "Kd", "Qc", "9c", "9c"};
+        players=Poker.setCards(args,players,numplayers);
+        numplayers=players.size();
+        Poker.rank(numplayers,players);
+        Assert.assertEquals(players.get(0).getrank(),9);
+    }
+    @Test
+    public void testTripletsOnePlayer(){
+        String [] args={"3d", "3h","3s","7s","Qs", "4h", "Kd", "Qc", "7h", "7c"};
+        players=Poker.setCards(args,players,numplayers);
+        numplayers=players.size();
+        Poker.rank(numplayers,players);
+        Assert.assertEquals(players.get(0).getrank(),10);
+    }
+    @Test
+    public void testOverfullHouseOnePlayer(){
+        String [] args={"3c", "3h","3s","9s","Qs", "4h", "Kd", "Qc", "9c", "3d"};
+        players=Poker.setCards(args,players,numplayers);
+        numplayers=players.size();
+        Poker.rank(numplayers,players);
+        Assert.assertEquals(players.get(0).getrank(),11);
+    }
+    @Test
+    public void testHomoSapiensOnePlayer(){
+        String [] args={"jh", "qs","kd","js","Qh", "4h", "Kd", "Qc", "9c", "3d"};
+        players=Poker.setCards(args,players,numplayers);
+        numplayers=players.size();
+        Poker.rank(numplayers,players);
+        Assert.assertEquals(players.get(0).getrank(),12);
+    }
+    @Test
+    public void testKingdomOnePlayer(){
+        String [] args={"jh", "qh","kh","th","Qs", "4h", "Kd", "Qc", "9h", "3h"};
+        players=Poker.setCards(args,players,numplayers);
+        numplayers=players.size();
+        Poker.rank(numplayers,players);
+        Assert.assertEquals(players.get(0).getrank(),13);
+    }
+    @Test
+    public void testOrgyOnePlayer(){
+        String [] args={"jc", "jh","js","9s","Qs", "4h", "Kd", "Qc", "qh", "3d"};
+        players=Poker.setCards(args,players,numplayers);
+        numplayers=players.size();
+        Poker.rank(numplayers,players);
+        Assert.assertEquals(players.get(0).getrank(),14);
+    }
+    @Test
+    public void testPoliticsOnePlayer(){
+        String [] args={"jh", "qh","kh","js","Qh", "4h", "Kd", "Qs", "ks", "3d"};
+        players=Poker.setCards(args,players,numplayers);
+        numplayers=players.size();
+        Poker.rank(numplayers,players);
+        Assert.assertEquals(players.get(0).getrank(),15);
+    }
+    @Test
+    public void testDinnerPartyOnePlayer(){
+        String [] args={"qh", "qd","ks","9s","Qs", "4h", "Kd", "Js", "kh", "3d"};
+        players=Poker.setCards(args,players,numplayers);
+        numplayers=players.size();
+        Poker.rank(numplayers,players);
+        Assert.assertEquals(players.get(0).getrank(),16);
+    }
+
 
 }
